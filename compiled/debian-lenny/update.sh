50f0fbbf37e5e2d06abde437d1b03fbfcf6b82c6
Paulo R. Zanoni <paulo@c3sl.ufpr.br>
2008-08-14 10:50:30 -0300

    Update.sh should get all generated debs

--- compiled/debian-lenny/update.sh
+++ compiled/debian-lenny/update.sh
@@ -5,11 +5,11 @@ set -e
 cd ../../mdm
 make clean
 make debian-lenny
-cp packages/mdm_0.0.1_i386.deb ../compiled/debian-lenny/
+cp packages/*.deb ../compiled/debian-lenny/
 
 cd ../extra-modes/xephyr-gdm/
 make clean
 make debian-lenny
-cp packages/mdm-xephyr-gdm_0.0.1_i386.deb ../../compiled/debian-lenny/
+cp packages/*.deb ../../compiled/debian-lenny/
 
 cd ../../dependencies/debian-lenny/
--- compiled/ubuntu-8.04/update.sh
+++ compiled/ubuntu-8.04/update.sh
@@ -5,11 +5,11 @@ set -e
 cd ../../mdm
 make clean
 make ubuntu-8.04
-cp packages/mdm_0.0.1_i386.deb ../compiled/ubuntu-8.04/
+cp packages/*.deb ../compiled/ubuntu-8.04/
 
 cd ../extra-modes/xephyr-gdm/
 make clean
 make ubuntu-8.04
-cp packages/mdm-xephyr-gdm_0.0.1_i386.deb ../../compiled/ubuntu-8.04/
+cp packages/*.deb ../../compiled/ubuntu-8.04/
 
 cd ../../dependencies/ubuntu-8.04/
